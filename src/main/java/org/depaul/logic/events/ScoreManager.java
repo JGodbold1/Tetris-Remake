cd1fcca144fff00ef62b3ab3b6f530817596f186
JGodbold1 <97115931+JGodbold1@users.noreply.github.com>
2023-02-16 22:16:16 -0600

    Initial Commit

---
+++ src/main/java/org/depaul/logic/events/ScoreManager.java
@@ -0,0 +1,54 @@
+package org.depaul.logic.events;
+
+import org.depaul.gui.GuiController;
+import org.depaul.logic.data.Score;
+
+public class ScoreManager {
+
+    private final GuiController viewGuiController;
+    private final Score score;
+
+    private int linesCleared;
+    private int nLinesToBonusPoints;
+    private int nBonusPoints;
+    private int nCurrentBonusPoints;
+
+    public ScoreManager(GuiController c, Score s) {
+        viewGuiController = c;
+        score = s;
+        linesCleared = 0;
+        nLinesToBonusPoints = 10;
+        nBonusPoints = 40;
+        //Set to 0 because on first line cleared
+        //it will increase to nBonusPOints
+        nCurrentBonusPoints = 0;
+    }
+
+    public void addBrickScore(int n){
+        //Prevent scores of 0 from showing to keep
+        //logic in other classes simple
+        if(n == 0) return;
+
+        //Each brick worth 10 points for a baseline
+        int points = n * 10;
+
+        viewGuiController.ShowScore(points);
+        score.add(points);
+    }
+
+    public void addLinesCleared(int n){
+        //Prevent lines of 0 from showing to keep
+        //logic in other classes simple
+        if(n == 0) return;
+
+        int points = 0;
+        for(int i = 0; i < n; i++, points += nCurrentBonusPoints, linesCleared++){
+            //Every nLinesToBonusPoints number of lines cleared increase
+            //point value of clearing a line by nBonusPoints
+            if(linesCleared % nLinesToBonusPoints == 0) nCurrentBonusPoints += nBonusPoints;
+        }
+
+        viewGuiController.ShowScore(points);
+        score.add(points);
+    }
+}
